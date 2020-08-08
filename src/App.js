d722a0fadd124613ddff86dffe121d61b304dc82
Md Firoj Kabir <firoj.kabir65@gmail.com>
2020-08-08 12:29:02 +0200

    Import all the components to APP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import logo from './logo.svg'
+
 import './App.css'
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+// import Product from './components/Product'
+import Details from './components/Details'
+import Cart from './components/Cart'
+import Default from './components/Default'
 
 
 function App() {
 	return (
 		<>
-			<h1>Hello from App</h1>
+			<Navbar />
+			<ProductList />
+			<Details />
+			<Cart />
+			<Default />
 		</>
 	);
 }
