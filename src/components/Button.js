dc704237e1832dc952764eebbeb5aba566548843
Md Firoj Kabir <firoj.kabir65@gmail.com>
2020-08-08 19:21:44 +0200

    Use styled-component in Navbar

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 
 export const ButtonContainer = styled.button`
     text-transform: capitalize;
-    font-size: 1.4rem;
+    font-size: 1.2rem;
     background: transparent;
-    border: 0.1rem solid var(--lightBlue);
+    border: 0.05rem solid var(--lightBlue);
     color: var(--lightBlue);
     border-radius: 0.5rem;
     padding: 0.2rem 0.5rem;
