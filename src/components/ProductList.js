ffbab8f86ae365b793c884116264164bd1ebe70b
Md Firoj Kabir <firoj.kabir65@gmail.com>
2020-08-09 12:15:40 +0200

    Use context

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import Product from './Product'
 import Title from './Title'
 import { storeProducts } from '../data'
+import { ProductConsumer } from '../context'
 
 class ProductList extends Component {
     state={
@@ -8,14 +10,20 @@ class ProductList extends Component {
     }
 
     render() {
-        console.log(this.state.products)
-
         return (
             <>
                 <div className="py-5">
                     <div className="container">
                         <Title name="our " title="products" />
-                        <div className="row" />
+                        <div className="row">
+                            <ProductConsumer>
+                                {value => {
+                                    return value.products.map(product => {
+                                        return <Product key={product.id} product={product} />
+                                    })
+                                }}
+                            </ProductConsumer>
+                        </div>
                     </div>
                 </div>
             </>
