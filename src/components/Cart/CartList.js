0599132ec46a778d889813bcbedfc0258254e92b
Md Firoj Kabir <firoj.kabir65@gmail.com>
2020-08-18 17:06:18 +0200

    Collect & show products in CartItem page

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import CartItem from './CartItem'
 
-function CartList() {
+function CartList({ value }) {
+    const { cart } = value
+
     return (
-        <div>
-            Hello from cart item
-            
-            <CartItem />
+        <div className="container-fluid">
+            {cart.map(item => {
+                return <CartItem key={item.id} item={item} value={value} />
+            })}
         </div>
     )
 }
