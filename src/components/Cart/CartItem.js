a1318e16a477ea4b4aaa601c6dd8f174003fed39
Md Firoj Kabir <firoj.kabir65@gmail.com>
2020-08-18 19:14:39 +0200

    Work on total price in cartTotals & cartItem page

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -4,7 +4,7 @@ function CartItem({ item, value }) {
     const { id, title, img, price, total, count } = item
     const { increment, decrement, removeItem } = value
     return (
-        <div className="row my-1 text-capitalize text-center">
+        <div className="row my-2 text-capitalize text-center">
             <div className="col-10 mx-auto col-lg-2">
                 <img 
                     src={img}
@@ -39,6 +39,17 @@ function CartItem({ item, value }) {
                         +
                     </span>
                 </div>
+            </div>
+            <div className="col-10 mx-auto col-lg-2">
+                <div 
+                    className="cart-icon" 
+                    onClick={() => removeItem(id)}
+                >
+                    <i className="fas fa-trash"/>
+                </div>
+            </div>
+            <div className="col-10 mx-auto col-lg-2">
+                <strong>item total : $ {price}</strong>
             </div>
         </div>
     )
