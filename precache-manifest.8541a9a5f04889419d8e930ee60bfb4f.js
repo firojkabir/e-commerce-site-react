37af0f3e8385c123bea5d756e8e8b83780b41dce
Md Firoj Kabir <firoj.kabir65@gmail.com>
2023-04-27 11:19:55 +0200

    Updates

---
+++ precache-manifest.8541a9a5f04889419d8e930ee60bfb4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47433e137859dfb4167d881f9aed6d05",
+    "url": "/e-commerce-site-react/index.html"
+  },
+  {
+    "revision": "05dc47faa7cd577bc676",
+    "url": "/e-commerce-site-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "34a49b43094e11a64a60",
+    "url": "/e-commerce-site-react/static/css/main.f6d532ce.chunk.css"
+  },
+  {
+    "revision": "05dc47faa7cd577bc676",
+    "url": "/e-commerce-site-react/static/js/2.12c5a553.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-commerce-site-react/static/js/2.12c5a553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a49b43094e11a64a60",
+    "url": "/e-commerce-site-react/static/js/main.be98f642.chunk.js"
+  },
+  {
+    "revision": "7ce634ea6ec85b3eec36",
+    "url": "/e-commerce-site-react/static/js/runtime-main.89006365.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/e-commerce-site-react/static/media/logo.9f7e506d.svg"
+  }
+]);
